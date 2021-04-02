5be6e41bff989b71452e3d7e2ae561488eb076ba
Arnab Deb <57291806+ArnabDeb800@users.noreply.github.com>
2021-04-02 10:25:25 +0530

    Update string.java

--- string.java
+++ string.java
@@ -1,6 +1,6 @@
 public class ReverseString {
     public static void main(String[] args) {
-        String s1 = "neelendra";
+        String s1 = "ARNAB DEB";
         for(int i=s1.length()-1;i>=0;i--)
             {
                 System.out.print(s1.charAt(i));
